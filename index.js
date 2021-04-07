c70a2f2c345a8f5f8ff20195df7b6ea24dfb7186
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-04-07 00:27:33 -0400

    added in more

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 //Importing data and programs from  othere folders
 const inquirer = require("inquirer");
-const Manager = require("./lib/Manager.js");
-const Engineer = require("./lib/Engineer.js");
-const Intern = require("./lib/Intern.js");
-const displayPage = require("./src/displayPage.js");
+const mysql = require("mysql");
+const consoleTable = require("console.table");
+const util = require("util");
 
 const jobRank = ['Engineer', 'Intern','Manager']
 let employeeInfo = [];
 let employeesArray = [];
+
+//connection to sql server
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Blue rose",
+    database: "employees_DB"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    runSearch();
+});
+  
+
+  
+  
 
 //start the program 
 const startScreen = () =>{
