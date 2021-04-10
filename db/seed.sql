1e2fe05b3b8bdcebf5d401bd5aff85e03515e794
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-04-09 23:00:00 -0400

    added the information

---
+++ db/seed.sql
@@ -0,0 +1,35 @@
+USE employees_DB;
+
+-- adds into the employee table
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES(1, "Eren", "Jager", 1, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES(2, "Mikasa", "Ackerman", 1, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES(3, "Armin", "Alert", 2, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES(4, "Levi", "Ackerman", 3, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES(4, "Hange", "Zoe", 3, null);
+
+-- adds into the role table
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, "Fighter", 50000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (2, "Intellegence", 70000, 2);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (3, "Comander", 150000, 2);
+
+-- add into department 
+INSERT INTO department (id, name)
+VALUES (1, "Combat");
+
+INSERT INTO department (id, name)
+VALUES (2, "Intellegence");
+
